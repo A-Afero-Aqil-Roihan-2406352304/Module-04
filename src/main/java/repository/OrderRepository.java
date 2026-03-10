c8248a40c8e61aba8eb7a4d5c1612ac61a3d51ad
peroly <mernawatispeed08@gmail.com>
2026-03-10 11:24:06 +0700

    [GREEN] Implement OrderRepository class.

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -1,16 +1,47 @@
 package repository;
 
 import model.Order;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public class OrderRepository {
-    private List<Order> orderData = new ArrayList<Order>();
-    public Order save (Order order){return null;}
+    private List<Order> orderData = new ArrayList<>();
 
-    public Order findById(UUID id){return null;}
+    public Order save(Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(order.getId())) {
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i += 1;
+        }
 
-    public List<Order> findAllByAuthor(String author){return null;}
+        orderData.add(order);
+        return order;
+    }
+
+    public Order findById(UUID id) {
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(id)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 }
