c59f0b10a6bb5f405da73521ba0588f69f950e3e
peroly <mernawatispeed08@gmail.com>
2026-03-10 19:51:04 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/repository/PaymentRepository.java
+++ src/main/java/repository/PaymentRepository.java
@@ -12,14 +12,26 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        for (int i = 0; i < paymentData.size(); i++) {
+            if (paymentData.get(i).getId().equals(payment.getId())) {
+                paymentData.set(i, payment);
+                return payment;
+            }
+        }
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(UUID id) {
+        for (Payment payment : paymentData) {
+            if (payment.getId().equals(id)) {
+                return payment;
+            }
+        }
         return null;
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentData;
     }
 }
